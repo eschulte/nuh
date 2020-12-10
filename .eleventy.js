77efc43885925f99905deef7a4d6de39c95b8b5c
Eric Schulte <eschulte@grammatech.com>
2020-12-10 08:42:57 -0500

    Travis/CI infrastructure and barebones placeholder

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const config = {};
+
+module.exports = function(eleventyConfig) {
+  // Customized Markdown Export.
+  let markdownIt = require("markdown-it");
+  let md = markdownIt({html: true})
+  md.use(require("markdown-it-anchor"));
+  eleventyConfig.setLibrary("md", md);
+
+  eleventyConfig.addPairedShortcode("markdown", (content) => {
+    return md.render(content);
+  });
+
+  eleventyConfig.addPassthroughCopy("img");
+
+  return config;
+};
---
+++ index.md
@@ -0,0 +1,4 @@
+---
+layout: default
+title: National Union of the Homeless
+---
