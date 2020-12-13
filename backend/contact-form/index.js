59d3ef1527d6938713aeb759c81d9c55c21a9457
Eric Schulte <eschulte@grammatech.com>
2020-12-12 22:58:51 -0500

    Sending email

--- backend/contact-form/index.js
+++ backend/contact-form/index.js
@@ -16,29 +16,32 @@ async function accessSecret(name) {
 ]
 
 const nodemailer = require('nodemailer');
-let mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: email_address,
-        pass: email_password
-    }
-});
 
-exports.contactForm = (req, res) => {
+exports.contactForm = async (req, res) => {
     // res.set('Access-Control-Allow-Origin', 'nationalunionofthehomeless.org');
     res.set('Access-Control-Allow-Origin', '*');
 
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: await email_address,
+            pass: await email_password
+        }
+    });
+
     let mailDetails = {
-        from: process.env.USER,
-        to: process.env.USER,
+        from: await email_address,
+        to: await email_address,
         subject: 'Test mail',
         text: 'Node.js testing mail for NUH.'
     };
 
     mailTransporter.sendMail(mailDetails, function(err, data) {
         if(err) {
+            console.log("ERROR: " + err)
             return res.status(200).send("Error: " + err)
         } else {
+            console.log("Success.")
             return res.status(200).send("Success.")
         }
     });
@@ -49,3 +52,7 @@ exports.helloSecret = async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.status(200).send("HELLO SECRET: " + await secret);
 }
+
+// Testing
+var mocks = require('node-mocks-http');
+exports.contactForm({}, mocks.createResponse())
