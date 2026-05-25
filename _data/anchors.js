e8dd85a81998e299725cd541e2602228a312bfd6
ccnobbli <christopher.nobblitt@gmail.com>
2026-05-25 14:48:40 -0400

    implement anchor dropdowns

--- _data/anchors.js
+++ _data/anchors.js
@@ -1,12 +1,19 @@
 // All icons are from https://fontawesome.com/icons?d=gallery
 module.exports = [
-    { text: "mission", link: "/#mission" },
-    { text: "history", icon: "fa-user", link: "/#history-(who-we-are)" },
-    { text: "events", icon: "fa-calendar", link: "/events" },
-    { text: "winter-offensives", icon: "fa-snowflake", link: "/winter-offensive" },
-    { text: "blog", icon: "fa-bullhorn", link: "/blog" },
-    { text: "locals", icon: "fa-hands-helping", link: "/#locals" },
-    { text: "donate", icon: "fa-donate", link: "/donate" },
-    { text: "join us", icon: "fa-envelope", link: "/contact" },
-    { text: "news", icon: "fa-newspaper", link: "/news" }
-  ];
+  { text: "mission", link: "/#mission" },
+  { text: "blog", icon: "fa-bullhorn", link: "/blog" },
+  { text: "in the news", icon: "fa-newspaper", link: "/news" },
+  { text: "About Us", 
+    icon: "fa-info-circle",
+    children: [
+      { text: "history", icon: "fa-user", link: "/#history-(who-we-are)" },
+      { text: "locals", icon: "fa-hands-helping", link: "/#locals" },
+      { text: "winter-offensives", icon: "fa-snowflake", link: "/winter-offensive" },
+
+    ]
+    },
+
+  { text: "donate", icon: "fa-donate", link: "/donate" },
+  { text: "join us", icon: "fa-envelope", link: "/contact" }
+
+];
