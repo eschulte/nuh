ed4a0a5612b033fdd25880ffa9d5974451449256
ccnobbli <christopher.nobblitt@gmail.com>
2026-06-01 21:32:59 -0400

    6 panther ps add

--- _data/anchors.js
+++ _data/anchors.js
@@ -4,7 +4,7 @@ module.exports = [
   { text: "blog", icon: "fa-bullhorn", link: "/blog" },
   { text: "resources", icon: "fa-hammer", link:"/resources",
     children: [
-      // { text: "6 Panther P's", icon: "fa-tools", link: "/#history-(who-we-are)" },
+      { text: "6 Panther P's: Tools for the Poor Organizing the Poor", icon: "fa-hammer", link: "/resources/6-panther-ps" },
       { text: "5 Ingredients in the Poor Organizing the Poor", icon: "fa-mortar-pestle", link: "/resources/5-ingredients/" },
       // { text: "4 C's of Leadership", icon: "fa-anvils", link: "/winter-offensive" },
 
